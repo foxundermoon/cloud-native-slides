e8b906ee3ab032ff5cee3fb2e1f648e114782c13
fox <fox@vvfox.com>
2019-02-20 11:55:39 +0800

    fix embed width and height

--- presentation/embed.js
+++ presentation/embed.js
@@ -1,19 +1,31 @@
 import React from "react";
 
 export default class Embed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
   componentDidMount() {
-    // document.querySelector("#myVideo").requestFullscreen();
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
   }
 
   componentWillUnmount() {
-    // document.documentElement.requestFullscreen();
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
   render() {
+    const { width, height } = this.state;
     return (
       <iframe
-        style={{ position: "fixed", left: 0, right: 0, top:20 }}
-        width="1920px"
-        height="890px"
+        style={{ position: "fixed", left: 0, right: 0, top: 20 }}
+        width={width}
+        height={height}
         src={this.props.src}
         frameBorder="0"
         allowFullScreen
